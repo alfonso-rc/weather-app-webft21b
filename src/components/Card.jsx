6778115f1f60640d3d7a57a53e9a61eb5c7d37fd
José Alfonso Rosas Caamaño <j.alfonsorc@gmail.com>
2022-01-16 23:16:31 -0600

    create component card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
-export default function Card(props) {
+export default function Card({max,min,name,img,onClose}) {
   // acá va tu código
-  return <div>Card Component</div>
+    return (
+  <div>
+      <button onClick={onClose}>x</button>
+      <h3>{name}</h3>
+      <div>
+        <span>Max</span>
+        <span>{max}</span>
+      </div>
+      <div>
+        <span>Min</span>
+        <span>{min}</span>
+      </div>
+      <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt='imagen del clima'/>
+  </div>
+  )
 };
