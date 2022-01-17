15dc431a641c7eab3d9e78cf37adbf4788895d42
José Alfonso Rosas Caamaño <j.alfonsorc@gmail.com>
2022-01-17 00:27:52 -0600

    refactor create component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
+import CardTemp from './CardTemp';
 
 export default function Card({max,min,name,img,onClose}) {
   // acá va tu código
+  function handleOnClose(){
+    if(typeof onClose === "function") onClose();
+  }
     return (
   <div>
-      <button onClick={onClose}>x</button>
+      <button onClick={handleOnClose}>x</button>
       <h3>{name}</h3>
-      <div>
-        <span>Max</span>
-        <span>{max}</span>
-      </div>
-      <div>
-        <span>Min</span>
-        <span>{min}</span>
-      </div>
+      <CardTemp label="Min" value={min} />
+      <CardTemp label="Max" value={max} />
       <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt='imagen del clima'/>
   </div>
   )
 };
+
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,6 +6,7 @@ export default function Cards({cities}) {
   // acá va tu código
   // tip, podés usar un map
   // pasamos las propiedades con destructurin de App.js a este lugar con Card
+  
   return ( 
   <div>
         {cities.map((city)=>(/* propiedades que estan en App.js*/
