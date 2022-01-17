7bf3ed2ed808cbfffa3b829e28bbfc5fea0b3a72
José Alfonso Rosas Caamaño <j.alfonsorc@gmail.com>
2022-01-16 23:41:40 -0600

    create component cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Card from './Card';
+//en cards renderizamos card.recibira un arreglo de ciudades 
 
-export default function Cards(props) {
+export default function Cards({cities}) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
-};
+  // pasamos las propiedades con destructurin de App.js a este lugar con Card
+  return ( 
+  <div>
+        {cities.map((city)=>(/* propiedades que estan en App.js*/
+        <Card 
+          key={city.id}
+          min={city.main.temp_min}
+          max={city.main.temp_max}
+          name={city.name}
+          img={city.weather[0].icon}
+          onClose={() => alert(city.name)}
+        />))}
+  </div>
+    )
+}
